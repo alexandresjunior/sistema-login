8d69bbd1718a6075cab7a46ac9d41883d51a5282
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-25 09:29:41 -0300

    Criação de hook e encapsulamento da aplicação

--- sistema-login-js/src/App.js
+++ sistema-login-js/src/App.js
@@ -1,12 +1,13 @@
+import { AuthContext } from './contexts/AuthContext';
 import RoutesApp from './routes';
 import GlobalStyle from './styles';
 
 const App = () => {
   return (
-    <>
+    <AuthContext.Provider>
       <RoutesApp />
       <GlobalStyle />
-    </>
+    </AuthContext.Provider>
   );
 }
 
