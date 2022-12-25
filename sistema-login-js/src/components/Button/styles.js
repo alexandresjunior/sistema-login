23097274edaf90d460782ab0696bcb9cd7a012d4
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-25 09:49:18 -0300

    Criação dos componentes de Input e Button

---
+++ sistema-login-js/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.input`
+    padding: 16px 20px;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    width: 100%;
+    cursor: pointer;
+    background-color: #046EE5;
+    color: white;
+    font-weight: 600;
+    font-size: 16px;
+    max-width: 350px;
+`;
