8bb007eef6fbeb2649a3905ac6769b2bc503ae22
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2022-12-25 10:24:53 -0300

    Definição da página de Home

--- sistema-login-js/src/pages/Home/index.js
+++ sistema-login-js/src/pages/Home/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import { useAuth } from "../../hooks/useAuth";
+import * as C from "./styles";
 
 const Home = () => {
-    return <div>Home</div>;
-}
+    const signOut = useAuth();
+    const navigate = useNavigate();
+
+    return (
+        <C.Container>
+            <C.Title>Home</C.Title>
+            <Button Text="Sair" onClick={() => [signOut(), navigate("/")]}>
+                Sair
+            </Button>
+        </C.Container>
+    );
+};
 
 export default Home;
